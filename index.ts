c1dd8a9b2ba1c8a9d182c2745a8e87b19b98afe1
Abdullah Al Mohammad <email.to.titu@gmail.com>
2024-03-04 14:15:26 +0600

    Adding a simple welcome message to root get request

--- index.ts
+++ index.ts
@@ -9,6 +9,9 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Fillout take-home app!');
+})
 
 app.get('/:formId/filteredResponses', async (req: Request, res: Response) => {
     try {
