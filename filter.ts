7a0a63873616f53d1e179df968296ea53bb8a259
Abdullah Al Mohammad <email.to.titu@gmail.com>
2024-03-05 00:47:42 +0600

    Fix on proper error handling and type checking while filtering

--- filter.ts
+++ filter.ts
@@ -3,9 +3,24 @@ import { Data, Filter, Question } from "./types"
 export function applyFilters(data: Data, filters: Filter[]): Question[] {
     return data.questions.filter((question) => {
         return filters.every((filter) => {
+            let questionValue, filterValue;
             const filterQuestion = question.id === filter.id;
             
             if (!filterQuestion) return false;
+
+            switch (question.type) {
+                case "DatePicker":
+                    questionValue = new Date(question.value!);
+                    filterValue = new Date(filter.value);
+                    break;
+                case "NumberInput":
+                    questionValue = Number(question.value!);
+                    filterValue = Number(filter.value);
+                    break;
+                default:
+                    questionValue = question.value!;
+                    filterValue = filter.value;
+            }
 
             switch (filter.condition) {
                 case "equals":
